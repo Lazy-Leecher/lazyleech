7267e93970c6751a62581a7d969e33a381168bc8
Lucky Jain <72547660+lostb053@users.noreply.github.com>
2021-06-27 14:55:47 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-touch aria2.log lazyleech.log
-tail -f aria2.log &
-tail -f lazyleech.log &
+touch aria2.log
+tail -f aria2.log
 aria2c --enable-rpc=true -j5 -x5 > aria2.log 2>&1 &
 python3 -m lazyleech > lazyleech.log 2>&1
