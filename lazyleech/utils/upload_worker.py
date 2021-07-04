7df86d5836e4765f851db3db7ccd953d4836a5aa
blank X <theblankx@protonmail.com>
2021-07-04 09:48:02 +0700

    Handle MessageNotModified

--- lazyleech/utils/upload_worker.py
+++ lazyleech/utils/upload_worker.py
@@ -27,7 +27,7 @@ import traceback
 from collections import defaultdict
 from natsort import natsorted
 from pyrogram.parser import html as pyrogram_html
-from pyrogram.errors.exceptions.bad_request_400 import MessageIdInvalid
+from pyrogram.errors.exceptions.bad_request_400 import MessageIdInvalid, MessageNotModified
 from .. import PROGRESS_UPDATE_DELAY, ADMIN_CHATS, preserved_logs, TESTMODE, SendAsZipFlag, ForceDocumentFlag, LICHER_CHAT, LICHER_STICKER, LICHER_FOOTER, LICHER_PARSE_EPISODE
 from .misc import split_files, get_file_mimetype, format_bytes, get_video_info, generate_thumbnail, return_progress_string, calculate_eta, watermark_photo
 
@@ -285,6 +285,8 @@ async def progress_callback(current, total, client, message, reply, filename, us
                     except MessageIdInvalid:
                         message_exists[reply.chat.id].discard(reply.message_id)
                         return
+                    except MessageNotModified:
+                        pass
                 prevtext = text
                 last_edit_time = time.time()
                 progress_callback_data[message_identifier] = last_edit_time, prevtext, start_time, user_id
