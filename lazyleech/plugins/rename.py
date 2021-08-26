55777bad21e967223d31875c6aef9908ec5e3269
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-08-27 00:10:20 +0530

    Update rename.py

--- lazyleech/plugins/rename.py
+++ lazyleech/plugins/rename.py
@@ -28,7 +28,8 @@ from ..utils.upload_worker import _upload_file
 
 @Client.on_message(filters.command(['rename', 'filerename']) & filters.chat(ALL_CHATS))
 async def rename(client, message):
-    command = message.command
+    text = message.command
+    command = text.pop(0).lower()
     if 'file' in command:
         flags = (ForceDocumentFlag,)
     else:
