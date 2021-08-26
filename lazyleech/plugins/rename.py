c278538bf1d54d520e183bffce5e6b3cf73b1ad5
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-08-26 23:34:21 +0530

    New Command: /rename

---
+++ lazyleech/plugins/rename.py
@@ -0,0 +1,68 @@
+# lazyleech - Telegram bot primarily to leech from torrents and upload to Telegram
+# Copyright (c) 2021 lazyleech developers <theblankx protonmail com, meliodas_bot protonmail com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import asyncio
+import html
+import math
+import os
+import re
+import time
+
+from pyrogram import Client, filters
+
+from .. import ALL_CHATS, help_dict, ForceDocumentFlag, SendAsZipFlag, PROGRESS_UPDATE_DELAY
+from ..utils.upload_worker import _upload_file
+
+
+@Client.on_message(filters.command(['rename', 'filerename']) & filters.chat(ALL_CHATS))
+async def rename(client, message):
+    text = (message.text or message.caption).split(None, 1)
+    command = text.pop(0).lower()
+    if 'file' in command:
+        flags = (ForceDocumentFlag,)
+    else:
+        flags = ()
+    c_time = time.time()
+    name = message.text.split(None, 1)[1]
+    available_media = ("audio", "document", "photo", "sticker", "animation", "video", "voice", "video_note")
+    download_message = None
+    for i in available_media:
+        if getattr(message, i, None):
+            download_message = message
+            break
+    else:
+        reply = message.reply_to_message
+        if not getattr(reply, 'empty', True):
+            for i in available_media:
+                if getattr(reply, i, None):
+                    download_message = reply
+                    break
+    if download_message is None:
+        await message.reply_text('Media required')
+        return
+    filepath = os.path.join(str(message.from_user.id), name)
+    msg = await message.reply_text('Downloading...')
+    await download_message.download(filepath)
+    await msg.edit_text("Uploading...")
+    await asyncio.sleep(PROGRESS_UPDATE_DELAY)
+    await _upload_file(client, message, msg, name, filepath, ForceDocumentFlag in flags)
+    await msg.edit_text("Renamed Files Successfully")
+    os.remove(filepath)
+
+help_dict['extras'] = ('Extras',
+'''<b>Rename</b>
+/rename <i>[replied media]</i> 
+/filerename <i>[replied media]</i>''')
