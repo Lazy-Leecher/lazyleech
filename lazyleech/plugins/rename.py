c7e84d164ef9ab844a8ff6b1d69d5d0ea8b930c6
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-08-27 10:25:04 +0530

    Update rename.py

--- lazyleech/plugins/rename.py
+++ lazyleech/plugins/rename.py
@@ -51,7 +51,7 @@ async def rename(client, message):
     if download_message is None:
         await message.reply_text('Media required')
         return
-    filepath = name
+    filepath = os.path.join(name)
     msg = await message.reply_text('Downloading...')
     await download_message.download(filepath)
     await msg.edit_text("Uploading...")
